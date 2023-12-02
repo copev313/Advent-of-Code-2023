afd976ac3771ebde6afdd36fe21c9c84bf1bb3a0
copev313 <copev313@gmail.com>
2023-12-02 16:32:31 -0500

    WIP: Started Day 2

--- README.md
+++ README.md
@@ -16,10 +16,4 @@ A collection of solutions developed solving the Advent of Code 2023.
 ## Table of Contents
 
 - [Day 1](https://adventofcode.com/2023/day/1): Trebuchet?! [ [Solution](solutions/day1.py) ]
-<!-- - [Day 2](https://adventofcode.com/2023/day/2): xxx [ [Solution](solutions/day2.py) ]
-- [Day 3](https://adventofcode.com/2023/day/3): xxx [ [Solution](solutions/day3.py) ]
-- [Day 4](https://adventofcode.com/2023/day/4): xxx [ [Solution](solutions/day4.py) ]
-- [Day 5](https://adventofcode.com/2023/day/5): xxx [ [Solution](solutions/day5.py) ]
-- [Day 6](https://adventofcode.com/2023/day/6): xxx [ [Solution](solutions/day6.py) ]
-- [Day 7](https://adventofcode.com/2023/day/7): xxx [ [Solution](solutions/day7.py) ] -->
-
+- [Day 2](https://adventofcode.com/2023/day/2): Cube Conundrum [ [Solution](solutions/day2.py) ] 
---
+++ solutions/day2.py
@@ -0,0 +1,96 @@
+"""
+    Day 2: Cube Conundrum
+"""
+from utils.input import get_input_data
+
+MAX_RED_CUBES = 12
+MAX_GREEN_CUBES = 13
+MAX_BLUE_CUBES = 14
+MAX_GAME_SUM = sum(range(1,101))
+
+
+def parse_input(filename: str) -> list[str]:
+    """Retrieves, parses, and formats the input data."""
+    lines = get_input_data(filename)
+    # Remove newline characters:
+    lines = list(map(lambda x: x.replace("\n", ""), lines))
+    results = []
+    for index, line in enumerate(lines):
+        rounds = line.split(":")[-1].strip().split(";")
+        parsed_record = {
+            "game": index + 1,
+            "rounds": [x.strip() for x in rounds],
+        }
+        results.append(parsed_record)
+    return results
+
+
+def create_rgb_tuples(data: list[dict]) -> list[tuple[int, int, int]]:
+    """Provided a list of dicts, parses each rounds attribute and returns an
+    RGB tuple for each round.
+    
+    Example
+    -------
+        "rounds": [
+            "1 blue, 2 green, 3 red",
+            "7 red, 8 green", 
+            "1 green, 2 red, 1 blue",
+            "2 green, 3 red, 1 blue",
+            "8 green, 1 blue"
+        ]
+
+        "rounds": [
+            (3, 2, 1),
+            (7, 8, 0),
+            (2, 1, 1),
+            (3, 2, 1),
+            (0, 8, 1),
+        ]
+    """
+    for record in data:
+        rounds_list = record["rounds"]
+        rgb_tuples = []
+        for rnd in rounds_list:
+            red, green, blue = 0, 0, 0
+            inventory_itms = rnd.split(",")
+            for itm in inventory_itms:
+                count, color = itm.strip().split(" ")
+                match color:
+                    case "red":
+                        red += int(count)
+                    case "green":
+                        green += int(count)
+                    case "blue":
+                        blue += int(count)
+            rgb_tuples.append((red, green, blue))
+        record["rounds"] = rgb_tuples
+    return data
+
+
+def part1(data: list[str]) -> int:
+    """Solves part 1 of the day's challenge."""
+    data = create_rgb_tuples(data)
+    total_sum = MAX_GAME_SUM
+    for record in data:
+        rounds_list = record["rounds"]
+        for rnd in rounds_list:
+            red, green, blue = rnd
+            # [CASE] If any of the colors exceed their respective max, skip:
+            if red > MAX_RED_CUBES or green > MAX_GREEN_CUBES or blue > MAX_BLUE_CUBES:
+                total_sum -= record["game"]
+                break
+    return total_sum
+
+
+def part2(data: list[str]) -> int:
+    """Solves part 2 of the day's challenge."""
+    pass
+
+
+if __name__ == "__main__":
+    input_data = parse_input("day2.txt")
+    solution1 = part1(input_data)
+    print(f"* Part 1 Solution: {solution1}") # 2505
+
+    solution2 = part2(input_data)
+    print(f"* Part 2 Solution: {solution2}")
