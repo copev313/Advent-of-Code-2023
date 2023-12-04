ce1475f8eb95881bfda9be9c203c5b730cd624de
Evan Cope <copev313@gmail.com>
2023-12-04 17:32:53 -0500

    Finished Day 2 Solution

--- solutions/day2.py
+++ solutions/day2.py
@@ -6,7 +6,6 @@ from utils.input import get_input_data
 MAX_RED_CUBES = 12
 MAX_GREEN_CUBES = 13
 MAX_BLUE_CUBES = 14
-MAX_GAME_SUM = sum(range(1,101))
 
 
 def parse_input(filename: str) -> list[str]:
@@ -28,12 +27,12 @@ def parse_input(filename: str) -> list[str]:
 def create_rgb_tuples(data: list[dict]) -> list[tuple[int, int, int]]:
     """Provided a list of dicts, parses each rounds attribute and returns an
     RGB tuple for each round.
-    
+
     Example
     -------
         "rounds": [
             "1 blue, 2 green, 3 red",
-            "7 red, 8 green", 
+            "7 red, 8 green",
             "1 green, 2 red, 1 blue",
             "2 green, 3 red, 1 blue",
             "8 green, 1 blue"
@@ -50,6 +49,7 @@ def create_rgb_tuples(data: list[dict]) -> list[tuple[int, int, int]]:
     for record in data:
         rounds_list = record["rounds"]
         rgb_tuples = []
+        # Loop through each rnd string (ex: "1 blue, 2 green, 3 red")
         for rnd in rounds_list:
             red, green, blue = 0, 0, 0
             inventory_itms = rnd.split(",")
@@ -70,7 +70,7 @@ def create_rgb_tuples(data: list[dict]) -> list[tuple[int, int, int]]:
 def part1(data: list[str]) -> int:
     """Solves part 1 of the day's challenge."""
     data = create_rgb_tuples(data)
-    total_sum = MAX_GAME_SUM
+    total_sum = sum(range(1, 101))
     for record in data:
         rounds_list = record["rounds"]
         for rnd in rounds_list:
@@ -84,13 +84,25 @@ def part1(data: list[str]) -> int:
 
 def part2(data: list[str]) -> int:
     """Solves part 2 of the day's challenge."""
-    pass
+    data = create_rgb_tuples(data)
+    powers_list = []
+    for record in data:
+        min_red, min_green, min_blue = 0, 0, 0
+        rounds_list = record["rounds"]
+        for rnd in rounds_list:
+            red, green, blue = rnd
+            min_red = max(min_red, red)
+            min_green = max(min_green, green)
+            min_blue = max(min_blue, blue)
+        powers_list.append(min_red * min_green * min_blue)
+    return sum(powers_list)
 
 
 if __name__ == "__main__":
     input_data = parse_input("day2.txt")
     solution1 = part1(input_data)
-    print(f"* Part 1 Solution: {solution1}") # 2505
+    print(f"* Part 1 Solution: {solution1}")  # 2505
 
+    input_data = parse_input("day2.txt")
     solution2 = part2(input_data)
-    print(f"* Part 2 Solution: {solution2}")
+    print(f"* Part 2 Solution: {solution2}")  # 70265
